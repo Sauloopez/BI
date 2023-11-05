76dfa84872a0d661a70fd12f3648313f9e56ef71
Sauloopez <sauleta.selb@gmail.com>
2023-11-04 19:36:44 -0500

    first commit

---
+++ BI/home/customForms.py
@@ -0,0 +1,37 @@
+from django import forms
+
+
+class LoginForm(forms.Form):
+    username = forms.EmailField(
+        label='Email address',
+        widget=forms.EmailInput(attrs={'class': 'form-control', 'id': 'username'})
+    )
+    password = forms.CharField(
+        label='Password',
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'id': 'password'})
+    )
+
+
+class RegistryForm(forms.Form):
+    username = forms.EmailField(
+        label='Email address',
+        widget=forms.EmailInput(attrs={'class': 'form-control', 'id': 'username'})
+    )
+    password1 = forms.CharField(
+        label='Password',
+        required=True,
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'id': 'password1'})
+    )
+    password2 = forms.CharField(
+        label='Password Confirmation',
+        required=True,
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'id': 'password2'})
+    )
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password1 = cleaned_data.get('password1')
+        password2 = cleaned_data.get('password2')
+
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Las contraseñas no coinciden.")
---
+++ BI/home/views.py
@@ -0,0 +1,59 @@
+from django.core.exceptions import ValidationError
+from django.db import IntegrityError
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
+from .customForms import LoginForm, RegistryForm
+
+# Create your views here.
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def signup(request):
+    method = request.method
+    if method == 'GET':
+        return render(request, 'signup.html',{
+            'form': RegistryForm
+        })
+    elif method == 'POST':
+        form = RegistryForm(request.POST)
+        if form.is_valid():
+            try:
+                form.clean()
+                new_user = User.objects.create_user(username=form.cleaned_data['username'],
+                                                    password=form.cleaned_data['password1'])
+                new_user.save()
+                return redirect('/login/')
+            except IntegrityError:
+                return render(request, 'signup.html',{
+                    'form': RegistryForm,
+                    'error': 'User Already Exists'
+                })
+        return render(request, 'signup.html', {
+            'form': RegistryForm,
+            'error': 'Passwords are not equals'
+        })
+
+
+def login_view(request):
+    if request.method == 'POST':
+        data = request.POST
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('/')
+
+    else:
+        form = AuthenticationForm()        
+    return render(request, 'login.html', {
+        'form': form
+    })
+    
